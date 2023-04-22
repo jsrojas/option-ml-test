f4d49297df48315b4eaf878bfb18e263c881d652
Juan Sebastian Rojas Melendez <juansrojas93@gmail.com>
2023-04-21 21:22:58 -0500

    :pushpin: Main file for the API created.

--- src/api.py
+++ src/api.py
@@ -1,9 +1,16 @@
+"""
+This script holds the main logic to deploy a FastAPI
+that allows to obtain predictions from an XGBoost
+classification model that predicts if a flight will have
+delays or not in a binary format (0 not delay - 1 delay)
+
+Author: Juan Sebastian Rojas Melendez
+"""
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+import os
 from pydantic import BaseModel
 from utils.utils import load_model, process_data
-import os
-#import my_model  # Import your machine learning model
 
 # Loading environment variables
 if os.path.exists('.env'):
@@ -12,6 +19,7 @@ if os.path.exists('.env'):
 
 # Loading path to the classification model
 PATH_TO_MODEL = os.getenv('PATH_TO_MODEL')
+PATH_TO_ENCODER = os.getenv('PATH_TO_ENCODER')
 
 # Creating the API
 app = FastAPI()
@@ -32,21 +40,35 @@ class Prediction(BaseModel):
 # Defining the prediction endpoint
 @app.post("/predict", response_model=Prediction)
 def predict(request: InputData):
+    """
+    This function is an endpoint that takes an input data request of a flight, 
+    loads a classification model, processes the data to obtain an encoded dataframe 
+    and obtains a prediction from the model that is returned as a response from the API 
+    with the prediction.
+    
+    Args:
+      request (InputData): The input data for the prediction, which is an instance of 
+      the InputData
+    class.
+    
+    Returns:
+      a Prediction that is 0 if the flight will not have delays and 1 if it has delays.
+    """
     # Check if the request has all the class attributes
     if not any(hasattr(request, attr) for attr in vars(InputData)):
         # Raise an exception with code 412
         raise HTTPException(status_code=412, detail='the http request is empty or has missing data. Please review your request body')
     
     # Load the classification model
-    #classification_model = load_model(PATH_TO_MODEL)
+    classification_model = load_model(PATH_TO_MODEL)
     
-    # TODO aqui va la función de procesamiento de datos
-    df_encoded = process_data(request)
+    # Process the data to obtain the encoded df
+    df_encoded = process_data(request, PATH_TO_ENCODER)
 
-    # TODO aqui va la función de predicción
-    #result = classification_model.predict(df_encoded)
-    result = 1
+    # Obtain the prediction from the model
+    result = classification_model.predict(df_encoded)
+    
+    # Create the Prediction object for the response
     response = Prediction(prediction=result)
 
-    #return JSONResponse(content={'prediction':1})
     return response
